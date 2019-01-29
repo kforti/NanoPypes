e200ef3146e06f92fa0edc44bed9f837589b42bd
kevin fortier <kevinfortier@kevins-mbp.ad.umassmed.edu>
2019-01-29 13:36:36 -0500

    Tests for cluster scale

--- nanopypes/nanopypes.py
+++ nanopypes/nanopypes.py
@@ -5,17 +5,20 @@ from nanopypes.utils import temp_dirs, remove_temps, collapse_save
 def basecall(albacore, cluster):
     """ function for running the albacore basecaller in parallel on a cluster.
     """
-    cluster.connect_workers()
+    cluster.connect()
     func = albacore.build_func()
     input_path = Path(albacore.input_path)
     temp_path = input_path.joinpath("temp")
 
+    print("bins: ", albacore.bins)
     for bin in albacore.bins:
         dirs = temp_dirs(bin, albacore.input_path)
         commands = []
         for dir in dirs:
             commands.append(albacore.build_command(dir, bin.name))
+        print("commands: ", commands)
         cluster.map(func, commands)
+        cluster.show_progress()
 
         remove_temps(temp_path)
     collapse_save(albacore.save_path)
---
+++ tests/test_basecall.py
@@ -0,0 +1,26 @@
+import unittest
+import time
+from nanopypes.albacore import Cluster
+from nanopypes.nanopypes import *
+
+
+class TestBasecall(unittest.TestCase):
+    """Tests for the Albacore class."""
+
+    @classmethod
+    def setUp(self):
+        """Set up test fixtures, if any."""
+        pass
+
+    def tearDown(self):
+        """Tear down test fixtures, if any."""
+        pass
+
+
+    def test_000_basecall(self):
+        """Build a cluster object with yaml"""
+        yaml = "basecall_test_config.yml"
+        cluster = Cluster(config=yaml)
+        albacore = Albacore(input=yaml)
+
+        basecall(albacore, cluster)
