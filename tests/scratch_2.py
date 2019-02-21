f28fcee7c79653008caee793a7f0e981b46394fb
kevin fortier <kevinfortier@kevins-mbp.ad.umassmed.edu>
2019-02-21 12:19:57 -0500

    util objects

--- tests/scratch_2.py
+++ tests/scratch_2.py
@@ -1,1 +1,8 @@
-print(range(10))
+import csv
+
+
+
+with open('test_data/basecall_files/seq_sums/sum_combined.txt', 'r') as src_file:
+    csv_reader = csv.reader(src_file, delimiter='\t')
+    for i, line in enumerate(csv_reader):
+        print(line)
