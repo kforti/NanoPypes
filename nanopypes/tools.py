50f2922df36367beb55f5ecad043f4627ddfca8a
kevin fortier <kevinfortier@kevins-MacBook-Pro.local>
2019-03-11 23:15:33 -0400

    Basecall remote tests

--- nanopypes/tools.py
+++ nanopypes/tools.py
@@ -3,11 +3,13 @@ from nanopypes.oxnano import Albacore
 from nanopypes.pipes import AlbacoreBasecall
 
 
-def basecall(config, basecaller='Albacore'):
+def basecall(config, data_splits, basecaller='Albacore'):
     if basecaller == 'Albacore':
         albacore = Albacore(config)
-        cluster = Cluster(config)
-        basecaller = AlbacoreBasecall(albacore=albacore, compute=cluster)
-        data = basecaller.execute()
+        compute = Cluster(config)
+        compute.connect()
+        basecaller = AlbacoreBasecall(albacore=albacore, compute=compute, data_splits=data_splits)
+        basecalled_data = basecaller.execute()
+        basecaller.remove_parallel_data()
 
-    return data
+    return basecalled_data
