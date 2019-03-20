dd26851dc6202c12f35ef172cbfe575e0690bbbb
kevin fortier <kevinfortier@kevins-mbp-3.ad.umassmed.edu>
2019-03-20 14:50:07 -0400

    parallel batch processing albacore pipe

--- nanopypes/pipes.py
+++ nanopypes/pipes.py
@@ -44,7 +44,7 @@ class Pipe(ABC):
 
 
 class AlbacoreBasecall(Pipe):
-    def __init__(self, albacore, compute, data_splits):
+    def __init__(self, albacore, compute, data_splits, parallel_batches):
         print("Configuring albacore pipe.... ")
         self.compute = compute
         self.albacore = albacore
@@ -52,11 +52,14 @@ class AlbacoreBasecall(Pipe):
         self.input_path = Path(self.albacore.input_path)
         self.save_path = Path(self.albacore.save_path)
         self.splits_path = self.input_path.joinpath("split_data")
-        self.splits = data_splits
+        self.parallel_batches = parallel_batches
+        self.splits = data_splits * self.parallel_batches
 
     def execute(self):
         batch_counter = 0
         batches = len(self.albacore.batches)
+        commands = []
+        maps = 1
         for batch in self.albacore.batches:
             batch_counter += 1
             split_data(data_path=batch,
@@ -64,17 +67,17 @@ class AlbacoreBasecall(Pipe):
                        splits=self.splits,
                        compute=self.compute)
 
-            commands = []
             for split in range(self.splits):
                 commands.append(self.albacore.build_command(str(self.splits_path.joinpath(str(split))), batch.name))
-            print("\nBatch ", batch_counter, " out of ", batches)
-            try:
-                self.compute.map(self.func, commands)
-            except Exception as e:
-                print(e)
-            #self.compute.show_progress()
+            if batch_counter == self.parallel_batches:
+                print("\nBatches ", (batch_counter * maps - self.parallel_batches), " - ", (batch_counter * maps), " out of ", batches)
+                try:
+                    self.compute.map(self.func, commands)
+                except Exception as e:
+                    print(e)
+                #self.compute.show_progress()
 
-            remove_splits(self.splits_path)
+                remove_splits(self.splits_path)
         basecalled_data = collapse_save(self.albacore.save_path)
         return #basecalled_data
 
