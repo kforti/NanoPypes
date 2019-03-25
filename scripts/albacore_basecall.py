556fe9d505c6abbcb4ef360bf680160546faf266
kevin fortier <kevinfortier@kevins-mbp-89.ad.umassmed.edu>
2019-03-25 16:20:38 -0400

    add batch bunching

--- scripts/albacore_basecall.py
+++ scripts/albacore_basecall.py
@@ -3,8 +3,7 @@ from nanopypes.tools import basecall
 if __name__ == '__main__':
     config = 'albacore_basecall.yml'
     bc_data = basecall(config,
-                       continue_on=True,
-                       last_batch=None,
-                       data_splits=100)
+                       data_splits=125,
+                       batch_bunch=100)
 
 
