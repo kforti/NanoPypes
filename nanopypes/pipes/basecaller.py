00041013b76b7ffee5db5ba3297eb79051b91e96
kevin fortier <kevinfortier@kevins-MacBook-Pro-676.local>
2019-04-04 11:01:10 -0400

    manage worker memory

--- nanopypes/pipes/basecaller.py
+++ nanopypes/pipes/basecaller.py
@@ -113,7 +113,7 @@ class AlbacoreBasecaller(Pipe):
             #print("submitting commands")
             commands = self.client.submit(get_command, i, batch.name, self.albacore.build_command, self.input_path, None, priority=-10)
             #print("submitting basecalls")
-            bc = self.client.submit(basecall, self.function, commands, [copy_files, commands], priority=10, resources={'MEMORY': 10e8})
+            bc = self.client.submit(basecall, self.function, commands, [copy_files, commands], priority=10, resources={'memory-limit': 10e8})
             rm_splits = self.client.submit(remove_splits, this_split_path, [bc], priority=-10)
             fire_and_forget(rm_splits)
 
