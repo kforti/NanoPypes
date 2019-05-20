34d65126ed5f257d1bcdb12318698e62923f38b9
kevin fortier <kevinfortier@kevins-mbp-676.ad.umassmed.edu>
2019-05-20 16:37:20 -0400

    Guppy remote test

--- nanopypes/pipes/worker_clients.py
+++ nanopypes/pipes/worker_clients.py
@@ -11,21 +11,16 @@ class SingularityClient(PipeClient):
 
     def __init__(self):
         self.client = Client
-        self.image = None
 
     def execute(self, command, image=None, link=None, pull=False, bind=None):
         if pull:
             self.image = self.pull(link)
-        stdout = self.client.execute(self.image, command, bind=bind)
-        print(stdout)
+        stdout = self.client.execute(image, command, bind=bind)
         return stdout
 
-    def pull(self, container_file=None):
-        if container_file == None:
-            container_file = self.container_file
-        self.image = self.client.pull(container_file)
-
-        return self.image
+    def pull(self, link):
+        image = self.client.pull(link)
+        return image
 
 if __name__ == '__main__':
     client = SingularityClient()
