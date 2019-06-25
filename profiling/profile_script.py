294bcf2f226da50a2d1d433a5b5a909b76f05a81
kevin fortier <kevinfortier@kevins-MacBook-Pro-676.local>
2019-06-24 22:19:50 -0400

    profile run3

--- profiling/profile_script.py
+++ profiling/profile_script.py
@@ -17,27 +17,27 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description='Profile Run.')
     parser.add_argument("--run")
-    parser.add_argument("--components", nargs='+', default=None)
+    parser.add_argument("--component", nargs='+', default=None)
     parser.add_argument("--num_batches", type=int)
     args = parser.parse_args()
     run = args.run
-    comps = args.components
+    comp = args.component
     num_batches = args.num_batches
 
     print("run: ", run)
-    print("components: ", comps)
+    print("components: ", comp)
     print("num_batches: ", num_batches)
 
     new_dir = input_data_path.joinpath("albacore_profile_{}".format(run))
-    batches = [input_data_path.joinpath(batch) for batch in os.listdir(str(input_data_path))[0:num_batches]]
-    print(len(batches), batches)
-
     if new_dir.exists() is False:
         new_dir.mkdir()
+        batches = [input_data_path.joinpath(batch) for batch in os.listdir(str(input_data_path))[0:num_batches]]
+    print(len(batches), batches)
+
 
     move_files(batches, new_dir)
     config = "profile_params"
-    pr = ProfileRun(name=run, config_path=config)
+    pr = ProfileRun(name=run, component=comp, config_path=config)
     pr.run()
 
 
