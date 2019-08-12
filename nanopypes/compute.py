114da249ff08edd4eb150c77fb39ec3f79629da1
kevin fortier <kevinfortier@kevins-mbp-676.ad.umassmed.edu>
2019-08-12 16:33:16 -0400

    fix bam merge, restructure pipelinebuilder

--- nanopypes/compute.py
+++ nanopypes/compute.py
@@ -111,7 +111,7 @@ class ClusterManager:
         minimum_workers = self.min_num_workers or int(0.5 * self.num_workers)
         #self._cluster.scale(self.num_workers)
         print("NUM_WORKERS: ", self.num_workers)
-        self._cluster.adapt(minimum=self.num_workers, maximum=self.num_workers)
+        self._cluster.adapt(minimum=0, maximum=self.num_workers)
 
         #self.cluster.scheduler
         return self.client
