1cc687ac1e2bf5dce8a24b60bce4c531fca6e47e
kevin fortier <kevinfortier@kevins-mbp-676.ad.umassmed.edu>
2019-08-14 12:59:27 -0400

    fix bam merge, restructure pipelinebuilder

--- scripts/push_code.py
+++ scripts/push_code.py
@@ -7,26 +7,37 @@ import pexpect
 import dotenv
 
 dotenv.load_dotenv("env/.env")
-PASSWORD = os.environ['LSF_PASSWORD']
+LSF_PASSWORD = os.environ['LSF_PASSWORD']
+SLURM_PASSWORD = os.environ['SLURM_PASSWORD']
 SSH_PASSWORD = os.environ['SSH_PASSWORD']
 
 
 parser = argparse.ArgumentParser(description='Push code to cluster.')
 parser.add_argument("-m")
 parser.add_argument("-b")
+parser.add_argument("-c")
 args = parser.parse_args()
 msg = args.m
 branch = args.b
+cluster = args.c
 
 subprocess.call(["git", "add", "--all"])
 subprocess.call(["git", "commit", "-m", msg])
 subprocess.call(["git", "push", "origin", branch])
 
-child = pexpect.spawn("ssh kf78w@ghpcc06.umassrc.org")
-r = child.expect("password:")
+if cluster == "lsf":
+    child = pexpect.spawn("ssh kf78w@ghpcc06.umassrc.org")
+    r = child.expect("password:")
+    child.sendline(LSF_PASSWORD)
+    child.expect("git 2.9.5 is located under")
+elif cluster == 'slurm':
+    child = pexpect.spawn("ssh -F ~/c3ddb-cluster/linux/config c3ddb01.mit.edu")
+    r = child.expect("Enter passphrase for key '/Users/kevinfortier/c3ddb-cluster/linux/c3ddb-key':")
+    child.sendline(SLURM_PASSWORD)
+    child.expect(" from c3ddb01.cm.cluster")
 
-child.sendline(PASSWORD)
-child.expect("git 2.9.5 is located under")
+
+
 child.sendline("cd bin/NanoPypes")
 time.sleep(5)
 cmd = "git pull origin {}".format(branch)
